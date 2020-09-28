5634c1672f3b233c4dca58348b663d512f4e4c65
Martin Axelsen <kharar@gmail.com>
2020-09-28 20:42:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,9 @@ function I2C_WriteRegister (addr: number, reg: number, val: number) {
     false
     )
 }
+function I2C_CrearRegisterBitMask (reg: number, mask: number) {
+    tmp = I2C_ReadRegister(MFRC522_I2C_Address, reg)
+}
 function I2C_ReadRegister (addr: number, reg: number) {
     pins.i2cWriteNumber(
     addr,
@@ -21,12 +24,19 @@ function I2C_ReadRegister (addr: number, reg: number) {
     )
     return pins.i2cReadNumber(addr, NumberFormat.UInt8LE, false)
 }
+let tmp = 0
+let MFRC522_I2C_Address = 0
 serial.redirectToUSB()
+MFRC522_I2C_Address = 40
 basic.forever(function () {
     serial.writeLine("PCD_WriteRegister(TxModeReg, 0x00);")
+    I2C_WriteRegister(MFRC522_I2C_Address, 18, 0)
     serial.writeLine("PCD_WriteRegister(RxModeReg, 0x00);")
+    I2C_WriteRegister(MFRC522_I2C_Address, 19, 0)
     serial.writeLine("PCD_WriteRegister(ModWidthReg, 0x26);")
-    serial.writeLine("")
+    I2C_WriteRegister(MFRC522_I2C_Address, 36, 38)
+    serial.writeLine("PCD_ClearRegisterBitMask(CollReg, 0x80);")
+    I2C_WriteRegister(MFRC522_I2C_Address, 36, 38)
     serial.writeLine("")
     serial.writeLine("")
     serial.writeLine("")
